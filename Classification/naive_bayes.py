6d3127308aea3e454fed57dc7c77324b4aedfa18
KacperKubara <kjk1u17@soton.ac.uk>
2018-12-23 12:24:46 +0000

    k-nn and naive_bayes classifiers added

---
+++ Classification/naive_bayes.py
@@ -0,0 +1,55 @@
+# K-NN classification with k-fold cross validation
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+
+# Read Data
+dataset = pd.read_csv("Social_Network_Ads.csv") 
+
+# Choose which features to use
+x = dataset.iloc[:, 2:4].values # Features 
+y = dataset.iloc[:, 4].values   # Output value
+
+# Split data into train and test dataset
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+
+# Data Preprocessing
+from sklearn.preprocessing import StandardScaler
+sc_x    = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test  = sc_x.transform(x_test)
+
+# Train Model
+from sklearn.naive_bayes import GaussianNB
+# That Model can perform online updates to model parameters via .partial_fit() method
+# Other models from naive_bayes package are BernoulliNB, MultinomialNB, ComplementNB
+classifier = GaussianNB()
+classifier.fit(x_train, y_train)
+
+# Predict Results
+y_pred = classifier.predict(x_test)
+
+# Measure accuracy with k-fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = x, y = y, cv = 10)
+mean_accuracy = accuracies.mean()
+standard_deviation = accuracies.std()
+
+# Visualise Results - code taken from www.superdatascience.com/machine-learning
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_test, y_test
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN Classification (Test set)')
+plt.xlabel('Age(scaled)')
+plt.ylabel('Estimated Salary(scaled)')
+plt.legend()
+plt.show()
--- Classification/svm.py
+++ Classification/svm.py
@@ -8,7 +8,7 @@ from sklearn.datasets import load_iris
 dataset = load_iris() 
 
 # Choose which features to use
-x = dataset["data"]   # It has 4 features - with PCA we will reduce it to 3 for 3D visualisation 
+x = dataset["data"]   # It has 4 features - with PCA we will reduce it to 2 for 2D visualisation 
 y = dataset["target"] # Output value
 
 # Split data into train and test dataset
@@ -17,7 +17,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rando
 
 # PCA to reduce dimensions to 3 (from 4)
 from sklearn.decomposition import PCA
-pca = PCA(n_components = 3, random_state = 42)
+pca = PCA(n_components = 2, random_state = 42)
 x_train = pca.fit_transform(x_train)
 x_test  = pca.transform(x_test)
 
@@ -37,26 +37,24 @@ y_pred = classifier.predict(x_test)
 
 # Measure accuracy with Cross Validation
 from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = x, y = y, cv = 10)
+accuracies = cross_val_score(classifier, x, y, cv = 10)
 mean_accuracy = accuracies.mean()
 standard_deviation = accuracies.std()
 
-# Merge output values with features into pandas dataframe
-# It is just used to make a plotting part clearer to read
-x_test  = sc_x.inverse_transform(x_test) # Return back to non-scaled values
-pred_df = pd.DataFrame({'x0': x_test[:,0], 'x1': x_test[:,1],'x2': x_test[:,2], 'y': y_pred})
-
-# Visualise Results
-from mpl_toolkits.mplot3d import axes3d
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlabel('X0')
-ax.set_ylabel('X1')
-ax.set_zlabel('X2')
-ax.set_title('Predicted Datapoints')
-temp_df = pred_df[pred_df['y'] == 0] # Take only rows from dataset containing y = 0
-ax.scatter(temp_df['x0'], temp_df['x1'], temp_df['x2'], color = 'r')
-temp_df = pred_df[pred_df['y'] == 1]
-ax.scatter(temp_df['x0'], temp_df['x1'], temp_df['x2'], color = 'g')
-temp_df = pred_df[pred_df['y'] == 2]
-ax.scatter(temp_df['x0'], temp_df['x1'], temp_df['x2'], color = 'b')
+# Visualise Results - code taken from www.superdatascience.com/machine-learning
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_test, y_pred
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Random Forest Classification (Test set)')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.legend()
+plt.show()
